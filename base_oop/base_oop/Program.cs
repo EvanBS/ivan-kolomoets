0ea0760f4adef6748b36a1c07736d5509ed6e2e9
EvanBS <evandiesp@gmail.com>
2019-03-30 22:08:16 +0200

    init

---
+++ base_oop/base_oop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace base_oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
